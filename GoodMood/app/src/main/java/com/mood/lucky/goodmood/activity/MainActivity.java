3ca4cac9008393c81e3be230970bfae12317bd50
Mihail Pantyuhin <mpantyuhin@buffergroup.com>
2017-09-12 18:03:18 +0300

    first commit

--- GoodMood/app/src/main/java/com/mood/lucky/goodmood/activity/MainActivity.java
+++ GoodMood/app/src/main/java/com/mood/lucky/goodmood/activity/MainActivity.java
@@ -2,14 +2,30 @@ package com.mood.lucky.goodmood.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.mood.lucky.goodmood.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnMyMood;
+    private TextView textMood;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textMood = (TextView) findViewById(R.id.textMood);
+        btnMyMood = (Button) findViewById(R.id.btnChangeMood);
+        btnMyMood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
